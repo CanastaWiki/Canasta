33e64ec3d29769408814992133731958f6d727aa
Pavel Astakhov <pastakhov@yandex.ru>
2021-08-18 02:35:55 +0600

    comment out command="WIKI=$wiki $command";

--- backup_scripts/backup.sh
+++ backup_scripts/backup.sh
@@ -7,7 +7,7 @@ wikis="de en es fr it ja ko pt ru sv zh"
 for wiki in $wikis; do
         echo "working on $wiki...";
         command="/backup_scripts/backup.db.sh wiki_$wiki";
-        command="WIKI=$wiki $command";
+        #command="WIKI=$wiki $command";
 
         echo "using $command";
         $command;
