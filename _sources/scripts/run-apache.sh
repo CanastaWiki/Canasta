b4abf95d2ad01e48478adafc656ecad9a689101f
Vedmaka <god.vedmaka@gmail.com>
2021-10-15 21:22:41 +0300

    Improves permissions checks for MW_VOLUME mount

--- _sources/scripts/run-apache.sh
+++ _sources/scripts/run-apache.sh
@@ -7,12 +7,40 @@ get_mediawiki_variable () {
     php /getMediawikiSettings.php --variable="$1" --format="${2:-string}"
 }
 
+dir_is_writable() {
+  # Use -L to get information about the target of a symlink,
+  # not the link itself, as pointed out in the comments
+  INFO=( $(stat -L -c "0%a %G %U" "$1") )
+  PERM=${INFO[0]}
+  GROUP=${INFO[1]}
+  OWNER=${INFO[2]}
+
+  if (( ($PERM & 0002) != 0 )); then
+      # Everyone has write access
+      return 0
+  elif (( ($PERM & 0020) != 0 )); then
+      # Some group has write access.
+      # Is user in that group?
+      if [[ $GROUP == $WWW_GROUP ]]; then
+          return 0
+      fi
+  elif (( ($PERM & 0200) != 0 )); then
+      # The owner has write access.
+      # Does the user own the file?
+      [[ $WWW_USER == $OWNER ]] && return 0
+  fi
+
+  return 1
+}
+
 # Soft sync contents from $MW_ORIGIN_FILES directory to $MW_VOLUME
 # The goal of this operation is to copy over all the files generated
 # by the image to bind-mount points on host which are bind to
-# $MW_VOLUME (./extensions, ./skins, ./config, ./images)
+# $MW_VOLUME (./extensions, ./skins, ./config, ./images),
+# note that this command will also set all the necessary permissions
 echo "Syncing files.."
 rsync -ah --inplace --ignore-existing --remove-source-files \
+  -og --chown=$WWW_GROUP:$WWW_USER --chmod=Fg=rw,Dg=rwx \
   "$MW_ORIGIN_FILES"/ "$MW_VOLUME"/
 
 # We don't need it anymore
@@ -28,12 +56,28 @@ if [ -e "$MW_VOLUME/config/LocalSettings.php"  ]; then
 fi
 
 # Permissions
-#chgrp -R "$WWW_GROUP" "$MW_VOLUME"
-chown -R "$WWW_GROUP":"$WWW_GROUP" "$MW_VOLUME"
-chmod -R g=rwX "$MW_VOLUME"
-#chgrp -R "$WWW_GROUP" /var/log/apache2
-chown -R "$WWW_GROUP":"$WWW_GROUP" /var/log/apache2
-chmod -R g=rwX /var/log/apache2
+# Note: this part if checking for root directories permissions
+# assuming that if the root directory has correct permissions set
+# it's in result of previous success run of this code or this code
+# was executed by another container (in case mount points are shared)
+# hence it does not perform any recursive checks and may lead to files
+# or directories down the tree having incorrect permissions left untouched
+
+echo "Checking permissions of $MW_VOLUME.."
+if dir_is_writable $MW_VOLUME; then
+  echo "Permissions are OK!"
+else
+  chown -R "$WWW_GROUP":"$WWW_GROUP" "$MW_VOLUME"
+  chmod -R g=rwX "$MW_VOLUME"
+fi
+
+echo "Checking permissions of $APACHE_LOG_DIR.."
+if dir_is_writable $APACHE_LOG_DIR; then
+  echo "Permissions are OK!"
+else
+  chown -R "$WWW_GROUP":"$WWW_GROUP" $APACHE_LOG_DIR
+  chmod -R g=rwX $APACHE_LOG_DIR
+fi
 
 autoinclude() {
   echo "Auto-include started.."
