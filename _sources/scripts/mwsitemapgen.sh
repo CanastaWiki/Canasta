f45edf75963a7fd539b57de217cb6c4ba1ad0085
Vedmaka <god.vedmaka@gmail.com>
2022-02-27 18:30:03 +0300

    Updates site url to handle protocol relative urls

--- _sources/scripts/mwsitemapgen.sh
+++ _sources/scripts/mwsitemapgen.sh
@@ -8,6 +8,12 @@ fi
 # Convert to seconds (suffixed sleep command has issues on OSX)
 SLEEPDAYS=$(expr $MW_SITEMAP_PAUSE_DAYS \* 60 \* 60 \* 24)
 
+SITE_SERVER=$MW_SITE_SERVER
+# Fallback to https:// scheme if it's protocol-relative
+if [[ $SITE_SERVER == "//"* ]]; then
+    SITE_SERVER="https:$SITE_SERVER"
+fi
+
 echo Starting sitemap generator...
 # Wait three minutes after the server starts up to give other processes time to get started
 sleep 30
@@ -22,8 +28,8 @@ while true; do
       --identifier=$MW_SITEMAP_IDENTIFIER
 
     # sending the sitemap to google
-    echo "sending to Google -> https://www.google.com/ping?sitemap=$MW_SITE_SERVER/$MW_SCRIPT_PATH/sitemap/$MW_SITEMAP_SUBDIR/sitemap-index-$MW_SITEMAP_IDENTIFIER.xml"
-    curl --silent "https://www.google.com/ping?sitemap=$MW_SITE_SERVER/$MW_SCRIPT_PATH/sitemap/$MW_SITEMAP_SUBDIR/sitemap-index-$MW_SITEMAP_IDENTIFIER.xml" > /dev/null
+    echo "sending to Google -> https://www.google.com/ping?sitemap=$SITE_SERVER$MW_SCRIPT_PATH/sitemap/$MW_SITEMAP_SUBDIR/sitemap-index-$MW_SITEMAP_IDENTIFIER.xml"
+    curl --silent "https://www.google.com/ping?sitemap=$SITE_SERVER$MW_SCRIPT_PATH/sitemap/$MW_SITEMAP_SUBDIR/sitemap-index-$MW_SITEMAP_IDENTIFIER.xml" > /dev/null
 
     # Wait some seconds to let the CPU do other things, like handling web requests, etc
     echo mwsitemapgen waits for "$SLEEPDAYS" seconds...
