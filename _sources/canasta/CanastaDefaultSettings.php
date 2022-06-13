afe2dc68471062b19c3452e03b16d167889841f8
Jeffrey Wang <j@mywikis.com>
2022-06-13 12:38:13 -0500

    Set  from MW_SITE_SERVER env var

--- _sources/canasta/CanastaDefaultSettings.php
+++ _sources/canasta/CanastaDefaultSettings.php
@@ -37,6 +37,8 @@ if ( !is_readable( $canastaLocalSettingsFilePath ) ) {
 
 // Canasta default settings below
 
+$wgServer = getenv( 'MW_SITE_SERVER' ) ?? 'https://localhost';
+
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
 ## For more information on customizing the URLs
