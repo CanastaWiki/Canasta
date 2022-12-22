1649078e432bd83941305c4f7c3511e299a74dc4
Yaron Koren <yaron57@gmail.com>
2022-12-22 12:34:53 -0800

    Add showSMTPWarning()

--- _sources/canasta/CanastaUtils.php
+++ _sources/canasta/CanastaUtils.php
@@ -22,3 +22,30 @@ function cfLoadSkin( $skinName ) {
 	echo "The following skin was loaded with cfLoadSkin, but needs to be loaded with wfLoadSkin instead: $skinName";
 	die();
 }
+
+/**
+ * Not exactly a utility function, but - show a warning to users if $wgSMTP is not set.
+ */
+$wgHooks['SiteNoticeAfter'][] = 'showSMTPWarning';
+function showSMTPWarning( &$siteNotice, Skin $skin ) {
+	global $wgSMTP;
+
+	if ( $wgSMTP !== false ) {
+		return true;
+	}
+	$title = $skin->getTitle();
+	if ( !$title->isSpecialPage() ) {
+		return true;
+	}
+	$specialPage = MediaWiki\MediaWikiServices::getInstance()
+		->getSpecialPageFactory()
+		->getPage( $title->getText() );
+	$canonicalName = $specialPage->getName();
+	// Only display this warning for pages that could result in an email getting sent.
+	if ( !in_array( $canonicalName, [ 'Preferences', 'CreateAccount', 'Emailuser' ] ) ) {
+		return true;
+	}
+
+	$siteNotice .= '<div class="warningbox"><big>Please note that mailing does not currently work on this wiki, because Canasta requires <a href="https://www.mediawiki.org/wiki/Manual:$wgSMTP">$wgSMTP</a> to be set in order to send emails.</big></div>';
+	return true;
+}
