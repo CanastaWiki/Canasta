3284cf140f7cab08ea22cafa113185dd585c7b6a
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-13 15:33:43 +0000

    Update inotifywait.sh

--- _sources/scripts/notify/inotifywait.sh
+++ _sources/scripts/notify/inotifywait.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-exts_detect() {
 inotifywait -m --no-newline -e create,moved_to,delete,moved_from --format '%e:%f%0' user-extensions | while IFS=: read -rd '' event file; do case $event in CREATE,ISDIR|MOVED_TO,ISDIR) ln -rsft extensions -- user-extensions/"$file" ;; DELETE,ISDIR|MOVED_FROM,ISDIR) ln -rsft extensions -- canasta-extensions/"$file" || rm -- "$file"; esac done
-}
-skins_detect() {
-inotifywait -m --no-newline -e create,moved_to,delete,moved_from --format '%e:%f%0' user-skins | while IFS=: read -rd '' event file; do case $event in CREATE,ISDIR|MOVED_TO,ISDIR) ln -rsft skins -- user-skins/"$file" ;; DELETE,ISDIR|MOVED_FROM,ISDIR) ln -rsft skins -- canasta-skins/"$file" || rm -- "$file"; esac done
-}
 
-exts_detect
-skins_detect
+inotifywait -m --no-newline -e create,moved_to,delete,moved_from --format '%e:%f%0' user-skins | while IFS=: read -rd '' event file; do case $event in CREATE,ISDIR|MOVED_TO,ISDIR) ln -rsft skins -- user-skins/"$file" ;; DELETE,ISDIR|MOVED_FROM,ISDIR) ln -rsft skins -- canasta-skins/"$file" || rm -- "$file"; esac done
