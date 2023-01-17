b650623b91a59d5e79afea386b9ae74939ef6421
Yaron Koren <yaron57@gmail.com>
2023-01-17 13:10:17 -0500

    Add header warning to some special pages if $wgSMTP not set (#194)
    * Add showSMTPWarning()
    
    * Add handling for $wgEnableEmail and $wgEnableUserEmail
    
    * Add fixes suggested by @pastakhov
    
    * Add missing semicolon
    
    * More fixes for SMTP warning code

--- _sources/canasta/CanastaUtils.php
+++ _sources/canasta/CanastaUtils.php
@@ -22,3 +22,33 @@ function cfLoadSkin( $skinName ) {
 	echo "The following skin was loaded with cfLoadSkin, but needs to be loaded with wfLoadSkin instead: $skinName";
 	die();
 }
+
+/**
+ * Not exactly a utility function, but - show a warning to users if $wgSMTP is not set.
+ */
+$wgHooks['SiteNoticeAfter'][] = function ( &$siteNotice, Skin $skin ) {
+	global $wgSMTP, $wgEnableEmail, $wgEnableUserEmail;
+
+	if ( !$wgEnableEmail || $wgSMTP ) {
+		return;
+	}
+	$title = $skin->getTitle();
+	if ( !$title->isSpecialPage() ) {
+		return;
+	}
+	$specialPage = MediaWiki\MediaWikiServices::getInstance()
+		->getSpecialPageFactory()
+		->getPage( $title->getText() );
+	$canonicalName = $specialPage->getName();
+	// Only display this warning for pages that could result in an email getting sent.
+	$specialPagesWithEmail = [ 'Preferences', 'CreateAccount' ];
+	if ( $wgEnableUserEmail ) {
+		$specialPagesWithEmail[] = 'Emailuser';
+	}
+	if ( !in_array( $canonicalName, $specialPagesWithEmail ) ) {
+		return;
+	}
+
+	$warningText = 'Please note that mailing does not currently work on this wiki, because Canasta requires <a href="https://www.mediawiki.org/wiki/Manual:$wgSMTP">$wgSMTP</a> to be set in order to send emails.';
+	$siteNotice .= Html::warningBox( '<span style="font-size: larger;">' . $warningText . '</span>' );
+};
