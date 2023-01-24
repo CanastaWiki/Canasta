c0463fb4ca1bd31dc5d9faa49f1e3c3b45345319
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-24 14:59:39 +0000

    Update inotifywait.sh

--- _sources/scripts/inotifywait.sh
+++ _sources/scripts/inotifywait.sh
@@ -11,6 +11,25 @@ canskins="$MW_HOME/canasta-skins"
 
 #Detects changes on the user-extensions and user-skins directories and symlinks to each directory skins and extensions else if removed it defaults back to
 #canasta-extensions or canasta-skins
-inotifywait -m -e create,moved_to,delete,moved_from --format '%e:%f%0' -- "$userexts"| while IFS=: read -r event file; do case $event in CREATE,ISDIR|MOVED_TO,ISDIR) ln -rsft "$extensions" -- "$userexts"/"$file" ;; DELETE,ISDIR|MOVED_FROM,ISDIR) echo "event: ${event}   file: ${file}"; ln -rsft "$extensions" -- "$canexts"/"$file" || rm -- "$file"; esac done
 
-inotifywait -m -e create,moved_to,delete,moved_from --format '%e:%f%0' -- "$userskins" | while IFS=: read -r event file; do case $event in CREATE,ISDIR|MOVED_TO,ISDIR) ln -rsft skins -- "$userskins"/"$file" ;; DELETE,ISDIR|MOVED_FROM,ISDIR) echo "event: ${event}   file: ${file}"; ln -rsft skins -- "$canskins"/"$file" || rm -- "$file"; esac done
+inotifywait -m -e create,moved_to,delete,moved_from --format '%e:%f%0' -- "$userexts" | 
+	while IFS=: read -r event file; do 
+		case $event in 
+			CREATE,ISDIR|MOVED_TO,ISDIR) 
+				ln -rsft "$extensions" -- "$userexts"/"$file" ;; 
+			DELETE,ISDIR|MOVED_FROM,ISDIR) 
+				echo "event: ${event} file: ${file}"; 
+				ln -rsft "$extensions" -- "$canexts"/"$file" || rm -- "$file";
+		esac 
+	done
+
+inotifywait -m -e create,moved_to,delete,moved_from --format '%e:%f%0' -- "$userskins" | 
+	while IFS=: read -r event file; do 
+		case $event in 
+			CREATE,ISDIR|MOVED_TO,ISDIR) 
+				ln -rsft skins -- "$userskins"/"$file" ;; 
+			DELETE,ISDIR|MOVED_FROM,ISDIR) 
+				echo "event: ${event} file: ${file}"; 
+				ln -rsft skins -- "$canskins"/"$file" || rm -- "$file"; 
+		esac 
+	done
