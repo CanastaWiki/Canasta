0b0ba3d355f6f3fac216435e74c9347377307171
gitmapd <50059062+gitmapd@users.noreply.github.com>
2023-01-24 17:15:28 +0000

    Update inotifywait.sh

--- _sources/scripts/inotifywait.sh
+++ _sources/scripts/inotifywait.sh
@@ -12,7 +12,7 @@ canskins="$MW_HOME/canasta-skins"
 #  - Detects activity changes inside user-extensions and user-skins.                           
 #  - Adds symlinks from the those folders to the appropriate correspondent folders, user-extensions to extensions and user-skins to skins. 				       
 #  - As a fallback, if the administrator wants to go back to the default extension provided by canasta-extensions folder, the moment the extension
-#    is removed or moved inside user-extensions it will revert back to canasta-extensions.  
+#    is removed or moved from user-extensions it will revert back by adding a symlink to the extension located in canasta-extensions.  
 
 inotifywait -m -e create,moved_to,delete,moved_from --format '%e:%f%0' -- "$userexts" | 
 	while IFS=: read -r event file; do 
