26ac5fde4922bcf04781f824dbda328e23270301
freephile <greg.rundlett@gmail.com>
2023-03-29 00:18:42 -0400

    Add missing quotes for strings

--- _sources/canasta/CanastaDefaultSettings.php
+++ _sources/canasta/CanastaDefaultSettings.php
@@ -66,9 +66,9 @@ $wgSVGConverter = 'rsvg';
 # see https://www.mediawiki.org/wiki/Manual:$wgCdnServersNoPurge
 $wgUseCdn = true;
 $wgCdnServersNoPurge = [];
-$wgCdnServersNoPurge[] = 10.0.0.0/8;     // 10.0.0.0 – 10.255.255.255
-$wgCdnServersNoPurge[] = 172.16.0.0/12;  // 172.16.0.0 – 172.31.255.255
-$wgCdnServersNoPurge[] = 192.168.0.0/16; // 192.168.0.0 – 192.168.255.255
+$wgCdnServersNoPurge[] = '10.0.0.0/8';     // 10.0.0.0 – 10.255.255.255
+$wgCdnServersNoPurge[] = '172.16.0.0/12';  // 172.16.0.0 – 172.31.255.255
+$wgCdnServersNoPurge[] = '192.168.0.0/16'; // 192.168.0.0 – 192.168.255.255
 
 # Include user defined LocalSettings.php file
 require_once "$canastaLocalSettingsFilePath";
