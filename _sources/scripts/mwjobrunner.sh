a3e219ba09ac976213ec97c2793469413ddd86d6
Pavel Astakhov <pastakhov@yandex.ru>
2023-04-12 22:42:19 +0600

    Add MW_JOB_RUNNER_MEMORY_LIMIT=512M environment variable (#50)

--- _sources/scripts/mwjobrunner.sh
+++ _sources/scripts/mwjobrunner.sh
@@ -17,18 +17,18 @@ while true; do
     date >> "$logfileNow"
     # Job types that need to be run ASAP mo matter how many of them are in the queue
     # Those jobs should be very "cheap" to run
-    php "$RJ" --type="enotifNotify" >> "$logfileNow" 2>&1
+    php "$RJ" --memory-limit="$MW_JOB_RUNNER_MEMORY_LIMIT" --type="enotifNotify" >> "$logfileNow" 2>&1
     sleep 1
-    php "$RJ" --type="createPage" >> "$logfileNow" 2>&1
+    php "$RJ" --memory-limit="$MW_JOB_RUNNER_MEMORY_LIMIT" --type="createPage" >> "$logfileNow" 2>&1
     sleep 1
-    php "$RJ" --type="refreshLinks" >> "$logfileNow" 2>&1
+    php "$RJ" --memory-limit="$MW_JOB_RUNNER_MEMORY_LIMIT" --type="refreshLinks" >> "$logfileNow" 2>&1
     sleep 1
-    php "$RJ" --type="htmlCacheUpdate" --maxjobs=500 >> "$logfileNow" 2>&1
+    php "$RJ" --memory-limit="$MW_JOB_RUNNER_MEMORY_LIMIT" --type="htmlCacheUpdate" --maxjobs=500 >> "$logfileNow" 2>&1
     sleep 1
     # Everything else, limit the number of jobs on each batch
     # The --wait parameter will pause the execution here until new jobs are added,
     # to avoid running the loop without anything to do
-    php "$RJ" --maxjobs=10 >> "$logfileNow" 2>&1
+    php "$RJ" --memory-limit="$MW_JOB_RUNNER_MEMORY_LIMIT" --maxjobs=10 >> "$logfileNow" 2>&1
 
     # Wait some seconds to let the CPU do other things, like handling web requests, etc
     echo mwjobrunner waits for "$MW_JOB_RUNNER_PAUSE" seconds... >> "$logfileNow"
