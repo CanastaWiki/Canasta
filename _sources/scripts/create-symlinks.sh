408c7e0b4fabd5c9b0909d03723f86285cc43ed2
Naresh Kumar <nk2indian@gmail.com>
2024-04-05 22:41:18 +0530

    Fix monitor-directories script and create-symlinks script (#366)
    * Fix monitor-directories script and create-symlinks script
    
    * Fix monitor-directories script and create-symlinks script
    
    * Upgrade Maps to include fix to work with VisualEditor (#367)
    
    The fix was added by @pastakhov .
    
    * Rename run-apache.sh to run-all.sh (#358)
    
    * Rename run-apache.sh to setup-all.sh
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Rename setup-all.sh to run-all.sh
    
    * Add sitemaps and images to robots.txt (#364)
    
    * Update mediawiki.conf
    
    * Rename robots.txt to robots-main.txt
    
    * Update Dockerfile
    
    * Create robots.php
    
    * Update robots-main.txt
    
    * Further fix for Maps with VE
    
    Another fix by @pastakhov. I don't know if this one is as critical as the first one.
    
    * Rebase with master
    
    * Resolve conflicts
    
    ---------
    
    Co-authored-by: Yaron Koren <yaron57@gmail.com>

--- _sources/scripts/create-symlinks.sh
+++ _sources/scripts/create-symlinks.sh
@@ -18,22 +18,12 @@ echo "Symlinking user extensions and overwriting any redundant bundled extension
 for user_extension_path in $(find $MW_HOME/user-extensions/ -maxdepth 1 -mindepth 1 -type d)
 do
   user_extension_id=$(basename $user_extension_path)
-  extension_symlink_path="$MW_HOME/extensions/$user_extension_id"
-  if [[ -e "$extension_symlink_path" ]]
-  then
-    rm "$extension_symlink_path"
-  fi
-  ln -s $MW_HOME/user-extensions/$user_extension_id/ $MW_HOME/extensions/$user_extension_id
+  ln -sf $MW_HOME/user-extensions/$user_extension_id/ $MW_HOME/extensions/$user_extension_id
 done
 
 echo "Symlinking user skins and overwriting any redundant bundled skins..."
 for user_skin_path in $(find $MW_HOME/user-skins/ -maxdepth 1 -mindepth 1 -type d)
 do
   user_skin_id=$(basename $user_skin_path)
-  skin_symlink_path="$MW_HOME/skins/$user_skin_id"
-  if [[ -e "$skin_symlink_path" ]]
-  then
-    rm "$skin_symlink_path"
-  fi
-  ln -s $MW_HOME/user-skins/$user_skin_id/ $MW_HOME/skins/$user_skin_id
+  ln -sf $MW_HOME/user-skins/$user_skin_id/ $MW_HOME/skins/$user_skin_id
 done
