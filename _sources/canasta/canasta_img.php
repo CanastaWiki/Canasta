9d79b5c55ec9affd880d14a5399fc4d3b671b3e1
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2025-04-29 19:54:02 +0530

    Replace Hooks with HookRunner for MW 1.43 (#7)
    * Update canasta_img.php
    
    * Update canasta_img.php

--- _sources/canasta/canasta_img.php
+++ _sources/canasta/canasta_img.php
@@ -45,6 +45,7 @@ function wfImageAuthMain() {
 
 	$services = \MediaWiki\MediaWikiServices::getInstance();
 	$permissionManager = $services->getPermissionManager();
+	$hookRunner = new \MediaWiki\HookContainer\HookRunner( $services->getHookContainer() );
 
 	$request = RequestContext::getMain()->getRequest();
 	$publicWiki = in_array( 'read', $services->getGroupPermissionsLookup()->getGroupPermissions( [ '*' ] ), true );
@@ -159,7 +160,7 @@ function wfImageAuthMain() {
 		// Run hook for extension authorization plugins
 		/** @var array $result */
 		$result = null;
-		if ( !Hooks::runner()->onImgAuthBeforeStream( $title, $path, $name, $result ) ) {
+		if ( !$hookRunner->onImgAuthBeforeStream( $title, $path, $name, $result ) ) {
 			wfForbidden( $result[0], $result[1], array_slice( $result, 2 ) );
 			return;
 		}
@@ -185,7 +186,7 @@ function wfImageAuthMain() {
 	}
 
 	// Allow modification of headers before streaming a file
-	Hooks::runner()->onImgAuthModifyHeaders( $title->getTitleValue(), $headers );
+	$hookRunner->onImgAuthModifyHeaders( $title->getTitleValue(), $headers );
 
 	// Stream the requested file
 	list( $headers, $options ) = HTTPFileStreamer::preprocessHeaders( $headers );
